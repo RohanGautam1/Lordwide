74d6ea41524645c5a62609633bd23a501a186555
RohanGautam1 <rohangautam.it19@gmail.com>
2022-05-16 12:44:07 +0530

    forgot page created.

--- src/component/SignIn.js
+++ src/component/SignIn.js
@@ -7,7 +7,7 @@ function SignIn() {
       <div class="container login-container my-5">
         <div class="row">
           <div class="col-md-6 welcome_auth">
-          {/* Add Image here */}
+            {/* Add Image here */}
           </div>
 
           <div class="offset-md-1 col-md-4 login-form">
@@ -15,20 +15,21 @@ function SignIn() {
               <div class="auth_branding">
                 <a class="auth_branding_in" href="https://procraft.studio"></a>
               </div>
-              <h1 class="auth_title text-left">Password Reset</h1>
+              <div className='fpass'>
+                <h1 class="auth_title text-left">Forgot password?</h1>
+              </div>
               <form>
-                <div class="alert alert-success bg-soft-primary border-0" role="alert">
-                  Enter your email address and we'll send you an email with instructions to reset your password.
+                <div class="alert  border-0" role="alert">
+                  Enter the email address you used when you joined and we'll send you instructions to reset your password.</div>
+                <div class="form-group">
+                  Your email
+                  <input type="email" class="form-control" name="email" placeholder="Tonynguyen@example.com" />
                 </div>
                 <div class="form-group">
-                  <input type="email" class="form-control" name="email" placeholder="Email Address" />
-                </div>
-                <div class="form-group">
-                  <button type="button" class="btn btn-primary btn-lg btn-block">Reset Password</button>
+                  <button type="button" class="btn btn-primary btn-lg btn-block">Submit</button>
                 </div>
                 <div class="form-group other_auth_links">
-                  <a class="" href="https://procraft.studio">Login</a>
-                  <a class="" href="https://procraft.studio">Register</a>
+                  <a class="" href="https://procraft.studio"> Back to sign in </a>
                 </div>
               </form>
             </div>
