bbc5412cc15a606391ef139f7b109abdffb434e9
Md Ali imam <imam.ali1751@gmail.com>
2022-05-28 11:36:35 +0530

    menupage resetpassword page basicinfo page

--- src/index.js
+++ src/index.js
@@ -13,6 +13,9 @@ import Forgot from "./component/Forgot"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './component/Dashboard';
 import Orders from './component/Orders';
+import MyMenu from './pages/MyMenu';
+import ResetPassword from './pages/ResetPassword';
+import BasicInformation from './pages/BasicInformation';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -27,6 +30,9 @@ root.render(
         <Route path="/Forgot" element={<Forgot />} />
         <Route path="/Dashboard" element={<Dashboard/>} />
         <Route path="/Orders" element={<Orders/>} />
+        <Route path="/menu" element={<MyMenu/>} />
+        <Route path="/resetpassword" element={<ResetPassword/>} />
+        <Route path="/userdetails" element={<BasicInformation/>} />
 
       </Routes>
     </Router>
